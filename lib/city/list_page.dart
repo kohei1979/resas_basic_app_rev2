413c71a85ae77b0b47497984127f8dd7c9a27b9c
kohei1979 <kohei1979@gmail.com>
2023-12-03 18:00:05 +0900

    FutureBuilderでインジケーターを表示

--- lib/city/list_page.dart
+++ lib/city/list_page.dart
@@ -24,25 +24,41 @@ class CityListPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('市区町村一覧'),
       ),
-      body: ListView(
-        children: [
-          //for文でcities配列の中身を一つずつ取り出し、ListTileで表示
-          for (final city in cities)
-            ListTile(
-              title: Text(city),
-              subtitle: const Text('政令指定都市'),
-              trailing: const Icon(Icons.navigate_next),
-              onTap: () {
-                Navigator.of(context).push<void>(
-                  MaterialPageRoute(
-                    builder: (context) => CityDetailPage(
-                      city: city,
+      body: FutureBuilder(
+        future: Future.delayed(const Duration(seconds: 3)),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            //非同期処理が完了したことを示す状態
+            case ConnectionState.done:
+              //元々のListviewを移動
+              return ListView(
+                children: [
+                  //for文でcities配列の中身を一つずつ取り出し、ListTileで表示
+                  for (final city in cities)
+                    ListTile(
+                      title: Text(city),
+                      subtitle: const Text('政令指定都市'),
+                      trailing: const Icon(Icons.navigate_next),
+                      onTap: () {
+                        Navigator.of(context).push<void>(
+                          MaterialPageRoute(
+                            builder: (context) => CityDetailPage(city: city),
+                          ),
+                        );
+                      },
                     ),
-                  ),
-                );
-              },
-            ),
-        ],
+                ],
+              );
+
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+            case ConnectionState.active:
+          }
+          //非同期処理が完了するまではインジケータを表示
+          return const Center(
+            child: LinearProgressIndicator(),
+          );
+        },
       ),
     );
   }
