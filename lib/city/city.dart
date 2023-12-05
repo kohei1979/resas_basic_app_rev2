8c5cdc35af7d215851033363738a9ace7c7d415f
kohei1979 <kohei1979@gmail.com>
2023-12-05 10:49:31 +0900

    detail.dartをStatefulWidgetに変更

--- lib/city/city.dart
+++ lib/city/city.dart
@@ -1,9 +1,11 @@
+import 'package:resas_project_app/city/city_type.dart';
+
 class City {
   City({
     required this.prefCode,
     required this.cityCode,
     required this.cityName,
-    required this.bigCityFlag,
+    required this.cityType,
   });
 
   // 今回の肝で、JSONを引数に取り中身をそれぞれ展開しCityクラスに変換して返却しています。
@@ -12,12 +14,12 @@ class City {
       prefCode: json['prefCode'] as int,
       cityCode: json['cityCode'] as String,
       cityName: json['cityName'] as String,
-      bigCityFlag: json['bigCityFlag'] as String,
+      cityType: CityType.values[int.parse(json['bigCityFlag'] as String)],
     );
   }
 
   int prefCode;
   String cityCode;
   String cityName;
-  String bigCityFlag;
+  CityType cityType;
 }
