0304166ade1fc741992971eef265dc9b65eb251b
kohei1979 <kohei1979@gmail.com>
2023-12-05 11:05:52 +0900

    initStateで一人当たり地方税のendpointを叩けるように変更

--- lib/city/detail_page.dart
+++ lib/city/detail_page.dart
@@ -1,16 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:resas_project_app/env.dart';
 
-class CityDetailPage extends StatelessWidget {
+class CityDetailPage extends StatefulWidget {
   const CityDetailPage({super.key, required this.city});
 
   final String city;
 
   @override
+  State<CityDetailPage> createState() => _CityDetailPageState();
+}
+
+class _CityDetailPageState extends State<CityDetailPage> {
+  late Future<String> _municipalityTaxesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    const host = 'opendata.resas-portal.go.jp';
+    const endpoint = 'opendata.resas-portal.go.jp';
+    final headers = {
+      'X-API-Key': Env.resasApiKey,
+    };
+
+    final param = {
+      'prefCode': '13',
+      'cityCode': '13115',
+    };
+
+    _municipalityTaxesFuture = http
+        .get(
+          Uri.https(host, endpoint, param),
+          headers: headers,
+        )
+        .then((res) => res.body);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(city)),
+      appBar: AppBar(title: Text(widget.city)),
       body: Center(
-        child: Text('$cityの詳細画面です'),
+        child: Text('${widget.city}の詳細画面です'),
       ),
     );
   }
